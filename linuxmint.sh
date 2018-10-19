27d7a461e3bf4e26068fbf893f9c2612070b25b5
dvs2101 <dvs2101@gmail.com>
2018-10-19 14:24:45 +0300

    initial

--- linuxmint.sh
+++ linuxmint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo -e "Enter User name, used for this PC:"
+echo -e "\033[1;33mEnter User name, used for this PC:\033[0m"
 read user
 echo -e "##############"
-echo -e "#\033[37;1;41mAdding Repos\033[0m#"
+echo -e "#\033[1;31mAdding Repos\033[0m#"
 echo -e "##############"
 dpkg --add-architecture i386
 su $user -c "wget -P /home/$user/ https://dl.winehq.org/wine-builds/Release.key && apt-key add /home/$user/Release.key"
@@ -10,14 +10,14 @@ apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial main' -
 add-apt-repository ppa:remmina-ppa-team/remmina-next -y
 apt update
 echo -e "#################"
-echo -e "#\033[37;1;41mInstalling Wine\033[0m#"
+echo -e "#\033[1;31mInstalling Wine\033[0m#"
 echo -e "#################"
 apt install --install-recommends winehq-stable -y
 echo -e "############################"
-echo -e "#\033[37;1;41mInstalling MC,Htop,Remmina\033[0m#"
+echo -e "#\033[1;31mInstalling MC,Htop,Remmina\033[0m#"
 echo -e "############################"
 apt install mc htop remmina remmina-plugin-rdp -y
 echo -e "###############################################"
-echo -e "#\033[37;1;41mDownloading Microsip in /home/$user directory\033[0m#"
+echo -e "#\033[1;31mDownloading Microsip in /home/$user directory\033[0m#"
 echo -e "###############################################"
 su $user -c "wget -P /home/$user/ https://www.microsip.org/download/MicroSIP-Lite-3.18.5.exe"
