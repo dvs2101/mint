aea52b036f7bbdab9f614ea367bb573b9a4f1b16
dvs2101 <dvs2101@gmail.com>
2018-10-19 14:13:50 +0300

    Update linuxmint.sh

--- linuxmint.sh
+++ linuxmint.sh
@@ -10,14 +10,14 @@ apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial main' -
 add-apt-repository ppa:remmina-ppa-team/remmina-next -y
 apt update
 echo -e "#################"
-echo -e "##\033[37;1;41mInstalling Wine\033[0m#"
+echo -e "#\033[37;1;41mInstalling Wine\033[0m#"
 echo -e "#################"
 apt install --install-recommends winehq-stable -y
 echo -e "############################"
-echo -e "##\033[37;1;41mInstalling MC,Htop,Remmina\033[0m#"
+echo -e "#\033[37;1;41mInstalling MC,Htop,Remmina\033[0m#"
 echo -e "############################"
 apt install mc htop remmina remmina-plugin-rdp -y
 echo -e "###############################################"
-echo -e "##\033[37;1;41mDownloading Microsip in /home/$user directory\033[0m#"
+echo -e "#\033[37;1;41mDownloading Microsip in /home/$user directory\033[0m#"
 echo -e "###############################################"
 su $user -c "wget -P /home/$user/ https://www.microsip.org/download/MicroSIP-Lite-3.18.5.exe"
