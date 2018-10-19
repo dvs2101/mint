9e470150818647adbe9353592fd65d9c574023e2
dvs2101 <dvs2101@gmail.com>
2018-10-19 15:53:41 +0300

    fdf

--- linuxmint.sh
+++ linuxmint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo  "\033[1;33mEnter User name, used for this PC:\033[0m"
+echo  "\033[1;33m>>Enter User name, used for this PC<<\033[0m"
 read user
 echo  "##############"
 echo  "#\033[1;31mAdding Repos\033[0m#"
@@ -23,6 +23,11 @@ echo  "#\033[1;31mDownloading Microsip in /home/$user directory\033[0m#"
 echo  "###############################################"
 su $user -c "wget -P /home/$user/ https://www.microsip.org/download/MicroSIP-Lite-3.18.5.exe"
 echo  "###############################################"
+echo  "#\033[1;31mAlmost done\033[0m"
 echo  "#\033[1;31mPlease follow further instructions for configuring Wine and MicroSIP!\033[0m#"
 echo  "###############################################"
-su $user -c "winecfg"
+su $user -c "winecfg && wine /home/$user/MicroSIP-Lite-3.18.5.exe"
+echo  "###############################################"
+echo  "#\033[1;31mWineconfig & MicroSIP installed!\033[0m#"
+echo  "###############################################"
+
